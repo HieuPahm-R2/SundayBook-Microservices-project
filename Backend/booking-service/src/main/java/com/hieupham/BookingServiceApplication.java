d15971dce4d58c4e6c6eed25610ff2b9a42c0e1c
HieuPahmTr <phamvanthang786@gmail.com>
2026-02-05 10:06:56 +0700

    feat(project): init payment service & booking service

---
+++ Backend/booking-service/src/main/java/com/hieupham/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hieupham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
